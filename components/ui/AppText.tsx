899e39d08e8e693c28128cc7cf2fbe1efe60d20d
Flufzak <khachatryan.suzi@gmail.com>
2026-01-05 17:05:00 +0100

    added font

---
+++ components/ui/AppText.tsx
@@ -0,0 +1,10 @@
+import { Text, TextProps } from "react-native";
+
+export default function AppText(props: TextProps) {
+  return (
+    <Text
+      {...props}
+      style={[{ fontFamily: "Montserrat_400Regular" }, props.style]}
+    />
+  );
+}
