a0a88a6e2e52b2ff7dd11c05eef4e65021d436ea
Flufzak <khachatryan.suzi@gmail.com>
2026-01-06 18:07:00 +0100

    theme colors modified

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,16 +1,20 @@
 import { palette } from "./palette";
 import type { RootState } from "../store/store";
+import { createSelector } from "@reduxjs/toolkit";
 
-export function selectColors(state: RootState) {
-  const isDark = state.theme.mode === "dark";
+export const selectColors = createSelector(
+  [(state: RootState) => state.theme.mode],
+  (mode) => {
+    const isDark = mode === "dark";
 
-  return {
-    primary: isDark ? palette.green : palette.pink,
-    secondary: isDark ? palette.blue : palette.orange,
+    return {
+      primary: isDark ? palette.green : palette.pink,
+      secondary: isDark ? palette.blue : palette.orange,
 
-    background: isDark ? "#3f3f3fff" : "#FFFFFF",
-    text: isDark ? "#FFFFFF" : "#3f3f3fff",
-    border: isDark ? "#2A2A2A" : "#E7E7E7",
-    card: isDark ? "#3f3f3fff" : "#FFFFFF",
-  };
-}
+      background: isDark ? "#3f3f3fff" : "#f5f5f5ff",
+      text: isDark ? "#FFFFFF" : "#3f3f3fff",
+      border: isDark ? "#2A2A2A" : "#FFFFFF",
+      card: isDark ? "#3f3f3fff" : "#FFFFFF",
+    };
+  }
+);
