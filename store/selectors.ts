96399f6cb41294e5b963e6c475b7ad886c090f8c
Flufzak <khachatryan.suzi@gmail.com>
2026-01-07 15:16:15 +0100

    updated selector

--- store/selectors.ts
+++ store/selectors.ts
@@ -1,13 +1,18 @@
+import { createSelector } from "@reduxjs/toolkit";
 import type { RootState } from "./store";
 
-export const selectCartItemsArray = (state: RootState) =>
-  Object.values(state.cart.items);
+const selectCartItemsMap = (state: RootState) => state.cart.items;
 
-export const selectTotalItems = (state: RootState) =>
-  Object.values(state.cart.items).reduce((sum, item) => sum + item.quantity, 0);
+export const selectCartItemsArray = createSelector(
+  [selectCartItemsMap],
+  (itemsMap) => Object.values(itemsMap)
+);
 
-export const selectSubtotal = (state: RootState) =>
-  Object.values(state.cart.items).reduce(
-    (sum, item) => sum + item.price * item.quantity,
-    0
-  );
+export const selectTotalItems = createSelector(
+  [selectCartItemsArray],
+  (items) => items.reduce((sum, i) => sum + i.quantity, 0)
+);
+
+export const selectSubtotal = createSelector([selectCartItemsArray], (items) =>
+  items.reduce((sum, i) => sum + i.price * i.quantity, 0)
+);
