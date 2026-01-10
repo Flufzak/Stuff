2a661afdcd304b7899bfa548a4d7c9a44db041d2
Flufzak <khachatryan.suzi@gmail.com>
2026-01-10 16:12:55 +0100

    modified card color

--- styles/theme.ts
+++ styles/theme.ts
@@ -15,7 +15,7 @@ export const selectColors = createSelector(
       background: isDark ? "#3f3f3fff" : "#f5f5f5ff",
       text: isDark ? "#FFFFFF" : "#3f3f3fff",
       border: isDark ? "#2A2A2A" : "#d6d5d5ff",
-      card: isDark ? "#3f3f3fff" : "#FFFFFF",
+      card: isDark ? "rgb(71, 70, 70)" : "#FFFFFF",
       tab: isDark ? "#363636ff" : "#FFFFFF",
       muted: isDark ? "#3f3f3fff" : "#535353ff",
       red: isDark ? "#c05151ff" : "#a82525ff",
